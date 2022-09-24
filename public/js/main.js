6aff8e032a762f3175f2e91698f5aec12c0f4f22
Jennifer Marshall <jennbmarshall@gmail.com>
2022-09-24 08:54:15 -0400

    update front-end js

--- public/js/main.js
+++ public/js/main.js
@@ -1,72 +1,76 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //delete button in ejs file
+const item = document.querySelectorAll('.item span') //item class in ejs
+const itemCompleted = document.querySelectorAll('.item span.completed') //intemCompleted class in ejs
 
-Array.from(deleteBtn).forEach((element)=>{
+Array.from(deleteBtn).forEach((element)=>{ //iterates over array of delete buttons and if button is clicked, will run deleteItem functin
     element.addEventListener('click', deleteItem)
 })
 
-Array.from(item).forEach((element)=>{
+Array.from(item).forEach((element)=>{ //iterates over array of incomplete items and if clicked, will run markComplete function
     element.addEventListener('click', markComplete)
 })
 
-Array.from(itemCompleted).forEach((element)=>{
+Array.from(itemCompleted).forEach((element)=>{ //iterates over array of completed items and if clicked, will run markUncomplete function
     element.addEventListener('click', markUnComplete)
 })
 
-async function deleteItem(){
+async function deleteItem(){ //runs if delete button is clicked
+    //parent node is a list, child node is list item.  grabbing text next to trashcan icon
     const itemText = this.parentNode.childNodes[1].innerText
     try{
-        const response = await fetch('deleteItem', {
+        const response = await fetch('deleteItem', { //trying to delete item
+            //looking for the speicific item to delete
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+            //this is itemText from above, grabbing from server and turning into json
+            body: JSON.stringify({ //turns string into a json
               'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() //turning json response into a variable
+        console.log(data) //console logging json data
+        location.reload() //reloads page
 
-    }catch(err){
+    }catch(err){ //if error will catch it and console it 
         console.log(err)
     }
 }
 
-async function markComplete(){
+async function markComplete(){ //function to mark item complete
+    //parent node is a list, child node is list item.  grabbing text 
     const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
+    try{ //will try to mark items complete using put method
+        const response = await fetch('markComplete', { //markComplete is the route on server.js
             method: 'put',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+            body: JSON.stringify({ //turns string into a json
                 'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() //holds json response in a variable 
+        console.log(data) //logs json
+        location.reload() //reloads page
 
-    }catch(err){
-        console.log(err)
+    }catch(err){ //if error, will catch
+        console.log(err) //logs error
     }
 }
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
+async function markUnComplete(){ //function to unmark completed items
+    const itemText = this.parentNode.childNodes[1].innerText //parent node is a list, child node is list item.  grabbing text
+    try{ //will try to uncheck completed items using a put method
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+            body: JSON.stringify({ //turns string into json
                 'itemFromJS': itemText
             })
           })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() //saves json in variable
+        console.log(data) //logs json
+        location.reload() //reloads page
 
-    }catch(err){
+    }catch(err){ //catches and logs error
         console.log(err)
     }
 }
